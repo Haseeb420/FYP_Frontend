33d5c9b33da36afad41cf0577ca2f7dff7b3cc66
Muhammad Haseeb Malik <63384019+Haseeb420@users.noreply.github.com>
2022-03-28 22:52:23 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'jquery';
+import 'bootstrap/dist/js/bootstrap.js';
+import React from 'react';
+import './App.css';
+import TMSHeader from './components/shared/TMSHeader';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TMSHeader />
     </div>
   );
 }
 
 export default App;
+
